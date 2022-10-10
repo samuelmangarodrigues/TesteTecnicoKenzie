0736078fa75aebdc0acff80856690c67a2b5aa79
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-10-10 10:02:41 -0300

    fix:CorsBug

--- todolist/settings.py
+++ todolist/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "todo",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -49,7 +52,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
+CORS_ALLOW_ALL_ORIGINS: True
 ROOT_URLCONF = "todolist.urls"
 
 TEMPLATES = [
