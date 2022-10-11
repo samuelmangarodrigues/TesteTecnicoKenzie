f52589248658d2d2045c0fa71a293c9d9d83b0d0
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-10-11 12:41:16 -0300

    fix:Fixed COrs

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from pickle import TRUE
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,10 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "todolist.urls"
-# CORS_ALLOW_ALL_ORIGINS: True
-CORS_ALLOWED_ORIGINS = [
-    "https://kenzietodo.vercel.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
