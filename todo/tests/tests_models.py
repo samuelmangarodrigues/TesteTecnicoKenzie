aa9d26512b186f62a077700e4386284984dd22ad
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-10-11 10:50:22 -0300

    fix:cors bug

--- todo/tests/tests_models.py
+++ todo/tests/tests_models.py
@@ -1,21 +1,21 @@
-# from pydoc import describe
-# from django.test import TestCase
-# from todo.models import Todo
+from pydoc import describe
+from django.test import TestCase
+from todo.models import Todo
 
-# class TodoTest(TestCase):
+class TodoTest(TestCase):
 
-#     @classmethod
-#     def setUpTestData(cls)-> None:
+    @classmethod
+    def setUpTestData(cls)-> None:
         
-#         cls.description="Primeira descrição"
-#         cls.todo_obj:Todo = Todo.objects.create(
-#             description= cls.description
-#         )
+        cls.description="Primeira descrição"
+        cls.todo_obj:Todo = Todo.objects.create(
+            description= cls.description
+        )
     
-#     def test_todo_fields(self):
+    def test_todo_fields(self):
 
-#         self.assertIsInstance(self.todo_obj, Todo)
-#         self.assertIsInstance(self.todo_obj.description,str)
-#         self.assertEqual(self.todo_obj.description,self.description)
-#         self.assertFalse(self.todo_obj.completed)
+        self.assertIsInstance(self.todo_obj, Todo)
+        self.assertIsInstance(self.todo_obj.description,str)
+        self.assertEqual(self.todo_obj.description,self.description)
+        self.assertFalse(self.todo_obj.completed)
     
